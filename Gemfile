e922b7725a59a7939f7c03f78d5f05700db266c9
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-26 20:58:01 +0800

    refactoring 8

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'colorize', '~> 1.1'
 gem 'pry', '~> 0.14.2'
-gem 'rspec', '~> 3.5'
+gem 'rspec', '~> 3.12'
+gem 'colorize', '~> 1.1'
 gem 'rubocop', '~> 1.57', '>= 1.57.2'
+
--- passwords_generator.rb
+++ passwords_generator.rb
@@ -1,5 +1,7 @@
+require 'colorize'
+
 class PasswordsGenerator
-  attr_reader :length, :number, :passwords
+  attr_reader :length, :number, :random_password, :passwords
 
   def initialize(length = 200, number = 100, passwords = [])
     @length = length
@@ -8,24 +10,27 @@ class PasswordsGenerator
   end
 
   def self.generate_and_print_passwords
-    ::PasswordsGenerator.new.generate_passwords.print_passwords
+    ::PasswordsGenerator.new.generate_passwords
   end
 
   def generate_passwords
-    (1..number).inject(@passwords) { |passwords, _n| passwords << random_password }
-
+    (1..number).inject(@passwords) { |passwords, _n| passwords << generate_print_return_random_password }
     self
   end
 
-  def random_password
-    (1..length).inject('') { |pass, _n| pass += shuffle.sample.to_s }
+  def generate_print_return_random_password
+    generate_random_password and print_random_password
   end
 
-  def print_passwords
-    passwords.each { |password| puts password }
+  def print_random_password
+    puts random_password.colorize(color: :green, mode: :bold) and return random_password
   end
 
   private
+
+  def generate_random_password
+    @random_password = (1..length).inject('') { |pass, _n| pass += shuffle.sample.to_s }
+  end
 
   def dictionary
     @dictionary ||= numbers + letters + symbols
