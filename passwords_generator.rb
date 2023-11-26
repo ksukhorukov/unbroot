6dac5c3a70c626256122cdf0cd19e0e72108555a
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-26 20:20:15 +0800

    refactoring 3

--- passwords_generator.rb
+++ passwords_generator.rb
@@ -10,17 +10,21 @@ class PasswordsGenerator
     @passwords = []
   end 
 
-  def generate_and_print_passwords
-    generate_passwords
-    print_passwords
+  def self.generate_and_print_passwords
+    ::PasswordsGenerator.new.send(:generate_passwords).send(:print_passwords)
   end 
 
   private
 
   def generate_passwords
     (1..number).inject(@passwords) { |passwords, n|  passwords << random_password }
+    return self
   end 
 
+  def random_password
+    (1..length).inject('') { |pass, n| pass += shuffle.sample.to_s }
+  end 
+  
   def print_passwords
     passwords.each { |password| puts password }
   end
@@ -43,10 +47,6 @@ class PasswordsGenerator
 
   def shuffle 
     dictionary.shuffle
-  end 
-
-  def random_password
-    (1..length).inject('') { |pass, n| pass += shuffle.sample.to_s }
   end 
 end 
 
