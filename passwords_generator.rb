bc3cf6a88358fdd55446689a92ff3b5c60cefd1e
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-26 20:17:07 +0800

    refactoring 2

---
+++ passwords_generator.rb
@@ -0,0 +1,52 @@
+require 'pry'
+
+class PasswordsGenerator
+  attr_reader :length, :number
+  attr_accessor :passwords
+
+  def initialize(length = 50, number = 50)
+    @length = length 
+    @number = number
+    @passwords = []
+  end 
+
+  def generate_and_print_passwords
+    generate_passwords
+    print_passwords
+  end 
+
+  private
+
+  def generate_passwords
+    (1..number).inject(@passwords) { |passwords, n|  passwords << random_password }
+  end 
+
+  def print_passwords
+    passwords.each { |password| puts password }
+  end
+
+  def dictionary 
+    @dictionary ||=  numbers + letters + symbols
+  end 
+
+  def numbers
+    @numbers ||= (0..9).to_a 
+  end 
+
+  def letters
+    @letters ||= ('A'..'z').to_a
+  end
+
+  def symbols
+    @symbols ||= ('!'..'?').to_a
+  end 
+
+  def shuffle 
+    dictionary.shuffle
+  end 
+
+  def random_password
+    (1..length).inject('') { |pass, n| pass += shuffle.sample.to_s }
+  end 
+end 
+
