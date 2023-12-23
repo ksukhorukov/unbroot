ea4a2a017fcf894f1da195bfc49ae0dbf54c4708
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-23 21:53:35 +0800

    colorized output

--- installer.sh
+++ installer.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
+PURPLE='\033[0;35m' 
+RED='\033[0;31m' 
+BLUE='\033[0;34m' 
+
+
 MAJOR_VERSION_PART_REQUIRED=3
+
+out() {
+  echo -e "$PURPLE $1"
+}
+
+info() {
+  echo -e "$BLUE $1"
+}
+
+error() {
+  echo -e "$RED $1"
+  exit
+}
 
 RUBY_INTERPRETATOR_LOCATION=`which ruby`
 
 if [[ "$RUBY_INTERPRETATOR_LOCATION" =~ ^[A-z0-9\/]+$ ]]; then
-  echo '[+] Perfect. You have some Ruby installed...'
+  out '[+] Perfect. You have some Ruby installed...'
 else 
-  echo '[-] ERROR! Ruby interpretator is not installed'
+  error '[-] ERROR! Ruby interpretator is not installed'
   exit
 fi
 
@@ -16,22 +34,26 @@ RUBY_VERSION=`ruby -v | awk '{print $2}'`
 MAJOR_VERSION_PART=`echo -e "$RUBY_VERSION" | cut -d. -f1`
 
 if [ $MAJOR_VERSION_PART -ge $MAJOR_VERSION_PART_REQUIRED ]; then 
-  echo -e "[+] Script can be installed. Your Ruby version is $RUBY_VERSION"
+  out "[+] Script can be installed. Your Ruby version is $RUBY_VERSION"
 else 
-  echo -e "$MAJOR_VERSION_PART"
-  echo "[-] ERROR! Ruby version mismatch. Minimal Ruby version $MAJOR_VERSION_PART_REQUIRED"
+  error "[-] ERROR! Ruby version mismatch. Minimal Ruby version $MAJOR_VERSION_PART_REQUIRED"
   exit
 fi 
 
 BUNDLER_LOCATION=`which bundler`
 
 if [ -z $BUNDLER_LOCATION ]; then
-  echo '[~] Installing bundler...'
+  info '[~] Installing bundler...'
   gem install bundler
 else 
-  echo '[+] Bundler already installed'
+  out '[+] Bundler already installed'
 fi
   
+out '[+] Installing gems...'
+
+bundle install
+
+
 
 
 
